2ed1a54be4551ec1a7c3bde0f6a7dfc44c1d427f
SanchoPanzsa <zelgadis4@gmail.com>
2022-01-11 23:56:21 +0300

    Initial commit

---
+++ src/io/sanchopansa/list/CircularLinkedList.java
@@ -0,0 +1,19 @@
+package io.sanchopansa.list;
+
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
+
+/**
+ *
+ * @author SanchoPansa
+ * 
+ * @param <T>
+ * @version 0.1
+ */
+public class CircularLinkedList<T> {
+
+
+}
---
+++ test/io/sanchopansa/list/CircularLinkedListTest.java
@@ -0,0 +1,28 @@
+package io.sanchopansa.list;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CircularLinkedListTest {
+    private String[] testArray = {
+            "Alice",
+            "Bob",
+            "Cinderella",
+            "Dennis",
+            "Eve",
+            "Franco"
+    };
+
+    private CircularLinkedList<String> cll;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void shiftListTest() {
+
+    }
+}
